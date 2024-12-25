8ce9e0b4bee421bf10d9aab174c99ee88b384f64
Mack Navickas <mjnavickas@gmail.com>
2024-12-24 18:17:10 -0600

    Added main website

---
+++ js/scroller.js
@@ -0,0 +1,39 @@
+let hero = document.querySelector('.hero-image') != null;
+const navbar = document.getElementById("navbar");
+
+if (hero) {
+  document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 100) { // Adjust scroll threshold as needed
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  });
+} else {
+  navbar.classList.add("scrolled");
+}
+
+
+
+function scrollToTarget(event, offset = 70) {
+  event.preventDefault();
+  const targetId = event.target.getAttribute('href'); // Get the href value
+  const targetElement = document.querySelector(targetId);
+
+  if (targetElement) {
+    const targetPosition = targetElement.offsetTop - offset; // Adjust scroll position
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth',
+    });
+  }
+}
+
+// Attach to multiple elements (e.g., navbar and other links)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    scrollToTarget(e); // Pass your desired offset
+  });
+});
